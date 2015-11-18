53de004345e4f5c946a5cd8cd803b387d7221405
Edwin Horneij <edwin@astekwallcovering.com>
2015-11-18 10:01:19 -0800

    Fixing display of inactive carousel items

--- app/models/spree/hero_carousel.rb
+++ app/models/spree/hero_carousel.rb
@@ -1,6 +1,6 @@
 module Spree
   class HeroCarousel < ActiveRecord::Base
     self.table_name = 'hero_carousels'
-    has_many :hero_carousel_items, -> { where active: true }
+    has_many :hero_carousel_items
   end
 end
