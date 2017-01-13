245adc4b4dea0b5e0ad991b47d01baa22ee9c1a1
joshlikesyou <joshlikesyou@gmail.com>
2017-01-13 15:00:55 -0800

    added compression to hero img

--- app/models/spree/hero_carousel_item.rb
+++ app/models/spree/hero_carousel_item.rb
@@ -6,8 +6,12 @@ module Spree
 
     default_scope {order('position')}
 
-    has_attached_file :large_image, styles: {large: '1140x485!'}
-    has_attached_file :small_image , styles: {small: '615x470!'}
+    has_attached_file :large_image,
+      styles: {large: '1140x485!'},
+      convert_options: { all: '-colorspace sRGB -quality 70'}
+    has_attached_file :small_image ,
+      styles: {small: '615x470!'},
+      convert_options: { all: '-colorspace sRGB -quality 70'}
 
     validates_attachment_content_type :large_image, content_type: /\Aimage\/.*\Z/
     validates_attachment_content_type :small_image, content_type: /\Aimage\/.*\Z/
