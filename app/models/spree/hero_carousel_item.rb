b786216e5fe0d3d1140cdedf776b13bba8f10ccc
joshlikesyou <joshlikesyou@gmail.com>
2017-01-16 11:50:45 -0800

    image compression

--- app/models/spree/hero_carousel_item.rb
+++ app/models/spree/hero_carousel_item.rb
@@ -8,14 +8,10 @@ module Spree
 
     has_attached_file :large_image,
       styles: {large: '1140x485!'},
-      convert_options: { all: '-colorspace sRGB -quality 70'},
-      url: ':s3_alias_url',
-      s3_host_alias: 'd2jd5cd93sjelr.cloudfront.net'
+      convert_options: { large: '-colorspace sRGB -quality 70'}
     has_attached_file :small_image,
       styles: {small: '615x470!'},
-      convert_options: { all: '-colorspace sRGB -quality 70'},
-      url: ':s3_alias_url',
-      s3_host_alias: 'd2jd5cd93sjelr.cloudfront.net'
+      convert_options: { : '-colorspace sRGB -quality 70'}
 
     validates_attachment_content_type :large_image, content_type: /\Aimage\/.*\Z/
     validates_attachment_content_type :small_image, content_type: /\Aimage\/.*\Z/
